a95bbbf0c367279aa1d20dbc42329699748af328
PDA-Khacer <59501767_PDA_Khacer@users.noreply.github.com>
2021-07-10 21:37:40 +0700

    done statics

---
+++ dangkytinchi2/dangkytinchi2/Controllers/ThongKeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dangkytinchi2.Controllers
+{
+    public class ThongKeController : Controller
+    {
+        // GET: ThongKe
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SinhVien()
+        {
+            return View("SinhVien");
+        }
+
+        public ActionResult GiaoVien()
+        {
+            return View("GiaoVien");
+        }
+
+        public ActionResult LopHocPhan()
+        {
+            return View("LopHocPhan");
+        }
+
+        public ActionResult HocPhan()
+        {
+            return View("HocPhan");
+        }
+
+        public ActionResult CanhCao()
+        {
+            return View("CanhCao");
+        }
+
+        public ActionResult HocBong()
+        {
+            return View("HocBong");
+        }
+
+        public ActionResult TotNghiep()
+        {
+            return View("TotNghiep");
+        }
+    }
+}
