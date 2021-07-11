e8955a023e2c9556623305cd74f237426e3488fa
NguyenTuanToan29051998 <tondola1998@gmail.com>
2021-07-11 11:49:51 +0700

    add toang

---
+++ dangkytinchi2/dangkytinchi2/Controllers/GiaoVienController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dangkytinchi2.Controllers
+{
+    public class GiaoVienController : Controller
+    {
+        // GET: GiaoVien
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: GiaoVien/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: GiaoVien/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GiaoVien/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GiaoVien/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: GiaoVien/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GiaoVien/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: GiaoVien/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ dangkytinchi2/dangkytinchi2/Controllers/LopChuyenNganhController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dangkytinchi2.Controllers
+{
+    public class LopChuyenNganhController : Controller
+    {
+        // GET: LopChuyenNganh
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: LopChuyenNganh/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: LopChuyenNganh/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LopChuyenNganh/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LopChuyenNganh/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: LopChuyenNganh/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LopChuyenNganh/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: LopChuyenNganh/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
